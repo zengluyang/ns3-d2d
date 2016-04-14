3aee3b91dc0cc8f0cc0f239922c02d6eb94505df
Luyang Zeng <zengluyang@gmail.com>
2016-04-14 22:23:53 +0800

    MOD: lte default eps threshold

--- src/evalvid/model/evalvid-server.cc
+++ src/evalvid/model/evalvid-server.cc
@@ -168,7 +168,7 @@ EvalvidServer::Setup()
       videoInfoStruct->frameSize = frameSize;
       videoInfoStruct->numOfUdpPackets = numOfUdpPackets;
       videoInfoStruct->packetInterval = Seconds(sendTime - lastSendTime);
-      //videoInfoStruct->packetInterval = Seconds((sendTime - lastSendTime)/500);
+      //videoInfoStruct->packetInterval = Seconds((sendTime - lastSendTime)/500); //threshold for lte
       m_videoInfoMap.insert (pair<uint32_t, m_videoInfoStruct_t*>(frameId, videoInfoStruct));
       NS_LOG_LOGIC(">> EvalvidServer: " << frameId << "\t" << frameType << "\t" <<
                                 frameSize << "\t" << numOfUdpPackets << "\t" << sendTime);
